3a264f4895578ce1181bb8a07a9f1df509eac48a
Andrew van Rooyen <wraith.andrew@gmail.com>
2015-07-25 22:20:19 +0200

    Moved to a simple auth mechanism (ssh)

--- docker/globus/run_myproxy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec /sbin/setuser myproxy /usr/sbin/myproxy-server
--- docker/globus/setup_user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-USER="grid"
-
-useradd -d / $USER
-echo "Added $USER user. Setting up myproxy."
-sudo -u myproxy myproxy-admin-adduser -c $USER -l $USER
-grid-mapfile-add-entry -dn $MYPROXY_SERVER_DN -ln $USER
-
