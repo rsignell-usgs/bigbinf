d476678b6233a54c1ed23b9c52e11eec1cfe11e0
Andrew van Rooyen <wraith.andrew@gmail.com>
2015-08-02 22:39:54 +0200

    Protocol tests now run multiple tests in one session and log output

--- protocol_tests/protocols.py
+++ protocol_tests/protocols.py
@@ -2,22 +2,39 @@
 Wrapper classes for transfer protocols
 """
 import subprocess
+import socket
+import fcntl
+import struct
 
-class GridFtp(object):
+PROC_ARGS = {"gridftp": "globus-url-copy -vb sshftp://%s%s %s",
+             "scp": "scp %s:%s %s"}
+
+class Protocol(object):
     """
-    Wrapper for globus gridftp
+    Wrapper for transfer protocols
     """
-    def __init__(self, remote_host, remote_path, local_path):
+    def __init__(self, remote_host, remote_path, local_path, protocol):
         self.remote_host = remote_host
         self.remote_path = remote_path
         self.local_path = local_path
+        self.protocol = protocol
+        self.proc_args = PROC_ARGS[protocol] % (remote_host, remote_path, local_path)
 
     def run(self):
         """
-        Run globus-url-copy
+        Copy the file using proc_args
         """
-        subprocess.call(["globus-url-copy",
-                         "-vb",
-                         "sshftp://%s%s" % (self.remote_host, self.remote_path),
-                         self.local_path])
+        print "\nCalling: %s\n" % self.proc_args
+        subprocess.call(self.proc_args.split(" "))
+
+def get_ip_address(ifname):
+    """
+    Gets the outward-facing, local ip address of the interface
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
--- protocol_tests/test.py
+++ protocol_tests/test.py
@@ -2,28 +2,45 @@
 Gets information on packets transferred during a globus-url-copy
 """
 import argparse
+from datetime import datetime
 import socket
 import sys
 
 from tcpdump import TcpDump
-from protocols import GridFtp
+from protocols import Protocol, get_ip_address
 
 def main():
     """
     Do a transfer and log the packet data
     """
-    remote_ip = socket.gethostbyname(args.host)
-    local_ip = socket.gethostbyname(socket.gethostname())
-    dump = TcpDump(args.interface, remote_ip, local_ip)
-    gridftp = GridFtp(args.host, args.remote_path, args.local_path)
+    gridftp = Protocol(args.host, args.remote_path, args.local_path, "gridftp")
+    scp = Protocol(args.host, args.remote_path, args.local_path, "scp")
+    test(gridftp)
+    test(scp)
 
-    # Run the test
+
+
+def test(protocol_obj):
+    """
+    Takes a Protocol object and runs a test for it
+    Outputs the dump to a file
+    """
+    remote_hostname = args.host
+    if "@" in remote_hostname:
+        remote_hostname = remote_hostname[remote_hostname.find("@")+1:]
+
+    remote_ip = socket.gethostbyname(remote_hostname)
+    local_ip = get_ip_address(args.interface)
+    dump = TcpDump(args.interface, remote_ip, local_ip)
+
     dump.start()
-    gridftp.run()
+    protocol_obj.run()
     dump.stop()
 
-    print dump.output
-
+    datestring = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    protocol_name = protocol_obj.protocol
+    with open("packet_dumps/%s_%s.dump" % (protocol_name, datestring), "w") as f:
+        f.writelines(line+"\n" for line in dump.output)
 
 
 if __name__ == "__main__":
