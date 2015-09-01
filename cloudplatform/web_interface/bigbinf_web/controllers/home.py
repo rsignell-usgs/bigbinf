72bb7f02e2a4a54c0f2279994127bbe5d2ba8b52
Brendan Ball <ball.brendan50@gmail.com>
2015-09-01 22:46:26 +0200

    added upload file api

--- cloudplatform/web_interface/bigbinf_web/controllers/home.py
+++ cloudplatform/web_interface/bigbinf_web/controllers/home.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, render_template, redirect, url_for, send_from_directory
+from flask import Blueprint, render_template, request
+from werkzeug import secure_filename
 
-homeBP = Blueprint("home",__name__)
+homeBP = Blueprint('home',__name__)
 
-@homeBP.route("/", methods=["GET"])
+@homeBP.route('/', methods=['GET'])
 def index():
 	#print url_for('homeBP.static', filename='layout.html')
-	#return render_template("index.html", title="Home")
+	#return render_template('index.html', title='Home')
 	return render_template('layout.html')
+
+
+@homeBP.route('/submitJob', methods=['POST'])
+def submitJob():
+	file = request.files['file']
+	if file:
+		filename = secure_filename(file.filename)
+		print(file.read())
+	return "well done", 201
