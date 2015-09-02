9d5af101fa465d3a7c9835a44c40c54709942180
Brendan Ball <ball.brendan50@gmail.com>
2015-09-02 11:01:30 +0200

    added angular file upload for job

--- cloudplatform/web_interface/bigbinf_web/controllers/home.py
+++ cloudplatform/web_interface/bigbinf_web/controllers/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify, abort
 from werkzeug import secure_filename
 
 homeBP = Blueprint('home',__name__)
@@ -10,10 +10,10 @@ def index():
 	return render_template('layout.html')
 
 
-@homeBP.route('/submitJob', methods=['POST'])
+@homeBP.route('/submitjob', methods=['POST'])
 def submitJob():
 	file = request.files['file']
 	if file:
 		filename = secure_filename(file.filename)
-		print(file.read())
-	return "well done", 201
+		print(filename)
+	return jsonify({'file': filename}), 201
