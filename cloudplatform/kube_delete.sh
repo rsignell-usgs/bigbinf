a9083f797780cdd59ee09168a84ce3604d60e714
Sanbi1 <sanbi1@bigbinf1.sanbi.ac.za>
2015-10-12 17:16:04 -0400

    changed kube local up and delete

--- cloudplatform/kube_delete.sh
+++ cloudplatform/kube_delete.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # kill and remove kubernetes containers
 docker ps | awk '{ print $1,$2 }' | grep gcr.io | awk '{print $1 }' | xargs -I {} docker kill {}
+docker ps | awk '{ print $1,$2 }' | grep bigbinf | awk '{print $1 }' | xargs -I {} docker kill {}
+docker ps | awk '{ print $1,$2 }' | grep kubernetes | awk '{print $1 }' | xargs -I {} docker kill {}
 
 docker ps -a | awk '{ print $1,$2 }' | grep gcr.io | awk '{print $1 }' | xargs -I {} docker rm {}
+
+docker ps | awk '{ print $1,$2 }' | grep kubernetes | awk '{print $1 }' | xargs -I {} docker rm {}
+docker ps -a | awk '{ print $1,$2 }' | grep bigbinf  | awk '{print $1 }' | xargs -I {} docker rm {}
